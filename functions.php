ac42f380d7647f0581de68341d77105e9cbc3dfa
Mikko Vatanen <mva@dhcp-wlan-10-0-112-250.portalify.com>
2012-10-12 15:56:09 +0300

    Initial commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Oxygen SPLL 2012
+ * @subpackage Functions
+ * @version 0.1
+ * @author Mikko Vatanen
+ * @copyright Copyright (c) 2012, Mikko Vatanen
+ * @link http://www.vapaatyyli.fi
+ * @license http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/* Add your own functions here. They will complement the original functions.php of the parent theme. */
+
+?>
